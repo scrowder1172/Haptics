47f517d06902f012fef7eb9b87e4d354665d0b47
Scott Crowder <scrowder@gmail.com>
2025-01-13 09:58:28 -0600

    Initial Commit

---
+++ Haptics/HapticsApp.swift
@@ -0,0 +1,20 @@
+//
+// File: HapticsApp.swift
+// Project: Haptics
+// 
+// Created by SCOTT CROWDER on 1/13/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct HapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
